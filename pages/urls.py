da51b79c6bdbb89cbf46c28e742227575abed261
Akash Ashok Soni <75109728+iamakashsoni@users.noreply.github.com>
2024-05-29 09:21:46 +0530

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include, re_path
 from pages import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path("", views.home, name='home'),
@@ -10,8 +9,3 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^uploads/(?P<path>.)$', serve, {'document_root': settings.MEDIA_ROOT}),
-        re_path(r'^static/(?P<path>.)$', serve, {'document_root': settings.STATIC_ROOT}),
-    ]
