8190890bcfbdc16b39d57b98828cc6850a746696
Akash Ashok Soni <75109728+iamakashsoni@users.noreply.github.com>
2024-06-08 15:01:32 +0530

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path('scan-qr-code-@08-06-2024/', views.qr_form_view, name='qr_form_view'),
     path('tinymce/', include('tinymce.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
