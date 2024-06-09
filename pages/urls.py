c08596cb10afa4913600c1c0de77fbb53b4ff7a8
komalrajbhar <komal.rajbhar@ratnamcollege.edu.in>
2024-06-09 19:37:20 +0530

    url updated

--- pages/urls.py
+++ pages/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", views.home, name='home'),
     path("passes", views.passes, name='passes'),
+    path('show_data/', views.show_data, name='show_data'),
+    path('download_data/', views.download_data, name='download_data'),
     path('scan-qr-code-@08-06-2024/', views.qr_form_view, name='qr_form_view'),
     path('tinymce/', include('tinymce.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
